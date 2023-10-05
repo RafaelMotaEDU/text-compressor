b62cf5655550670b41354d67d22377997718380a
Rafael Mota Alves <127457574+RafaelMotaEDU@users.noreply.github.com>
2023-10-04 22:38:08 -0300

    feat: created `sortCharacters` method

--- src/resolvers/methods.hpp
+++ src/resolvers/methods.hpp
@@ -8,3 +8,15 @@ void counterRepeatCharacters(string text, int asciiTable[]) {
     asciiTable[charIndex] +=1;
   }
 }
+
+template<typename T>
+void sortCharacters(Lue<T> &list, int asciiTable[]) {
+  for (int index = 0; index < ASCII_LENGTH; index++) {
+    if (asciiTable[index] > 0) {
+      Root<char> *root = new Root<char>;
+      root->data = char(index);
+      root->frequency = asciiTable[index];
+      insert(list, root);
+    }
+  }
+}
