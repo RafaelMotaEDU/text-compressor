9d39bfc139890f72cee76db1a892ef7253d69436
Rafael Mota Alves <127457574+RafaelMotaEDU@users.noreply.github.com>
2023-10-05 03:29:39 -0300

    fix: wrong commit

--- src/resolvers/methods.hpp
+++ src/resolvers/methods.hpp
@@ -21,29 +21,6 @@ void sortCharacters(Lue<T> &list, int asciiTable[]) {
   }
 }
 
-#include <iostream>
-
-using namespace std;
-
-void counterRepeatCharacters(string text, int asciiTable[]) {
-  for (int index = 0; index < text.length(); index++) {
-    const int charIndex = (int)text[index];
-    asciiTable[charIndex] +=1;
-  }
-}
-
-template<typename T>
-void sortCharacters(Lue<T> &list, int asciiTable[]) {
-  for (int index = 0; index < ASCII_LENGTH; index++) {
-    if (asciiTable[index] > 0) {
-      Root<char> *root = new Root<char>;
-      root->data = char(index);
-      root->frequency = asciiTable[index];
-      insert(list, root);
-    }
-  }
-}
-
 template<typename T>
 bool combineNodes(Lue<T>& list) {
     while(true){
@@ -84,5 +61,3 @@ bool combineNodes(Lue<T>& list) {
         }
     }
 }
-
-
