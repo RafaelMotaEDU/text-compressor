86e3ca67cb2c128a3acb6277e52d2dee427baa16
Rafael Mota Alves <127457574+RafaelMotaEDU@users.noreply.github.com>
2023-10-05 13:28:17 -0300

    refactor: refactoring `combineNodes` method and changing its return

--- src/resolvers/methods.hpp
+++ src/resolvers/methods.hpp
@@ -22,42 +22,27 @@ void sortCharacters(Lue<T> &list, int asciiTable[]) {
 }
 
 template<typename T>
-bool combineNodes(Lue<T>& list) {
-    while(true){
-        if (list.start == NULL || list.start->connect == NULL) {
-            return false;
-        }
-
+Root<T> *combineNodes(Lue<T>& list) {
+    while(!(list.start == NULL || list.start->connect == NULL)){
         Root<T> *firstValue = (list.start->root);
         Root<T> *secondValue = (list.start->connect->root);
-
+        Root<T> *newRoot = new Root<T>;
         Node<T> *firstNodeToRemove = list.start;
         Node<T> *secondNodeToRemove = list.start->connect;
 
-        Root<T> *newRoot = new Root<T>;
-
         const bool haveThirdConnect = list.start->connect->connect;
-        if(haveThirdConnect){
-            list.start = list.start->connect->connect;
-            delete firstNodeToRemove;
-            delete secondNodeToRemove;
-            newRoot->data = NULL;
-            newRoot->frequency = firstValue->frequency + secondValue->frequency;
-            newRoot->left = firstValue;
-            newRoot->right = secondValue;
-            insert(list, newRoot);
-        } else{
-            const bool haveNextConnect = list.start->connect;
-            if(haveNextConnect){
-                list.start = NULL;
-                delete firstNodeToRemove;
-                delete secondNodeToRemove;
-                newRoot->data = NULL;
-                newRoot->frequency = firstValue->frequency + secondValue->frequency;
-                newRoot->left = firstValue;
-                newRoot->right = secondValue;
-                insert(list, newRoot);
-            }
-        }
+
+        if(haveThirdConnect)list.start = list.start->connect->connect;
+        else list.start = NULL;
+
+        delete firstNodeToRemove;
+        delete secondNodeToRemove;
+
+        newRoot->data = NULL;
+        newRoot->frequency = firstValue->frequency + secondValue->frequency;
+        newRoot->left = firstValue;
+        newRoot->right = secondValue;
+        insert(list, newRoot);
     }
+    return list.start->root;
 }
